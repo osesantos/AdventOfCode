0df2487ee213639196623f2852ebff595c73d64b
osesantos <happycod3r@gmail.com>
2021-12-01 12:32:49 +0000

    first day

---
+++ 2021/src/main/scala/Day2.scala
@@ -0,0 +1,19 @@
+def day2() =
+  val input = "day2".getInput.map(s => s.toInt)
+  day2_1(input)
+  day2_2(input)
+
+def day2_1(input: List[Int]): Unit =
+  // Day 2 Part 1
+  val output = 0
+  println(s"Day 2 - PART 1: $output")
+
+def day2_2(input: List[Int]): Unit =
+  // Day 2 Part 1
+  val output = 0
+  println(s"Day 2 - PART 2: $output")
+
+
+
+
+
---
+++ 2021/src/main/scala/Utils.scala
@@ -0,0 +1,6 @@
+import scala.io.Source
+
+extension (str: String) {
+  def getInput: List[String] = Source.fromFile(s"src/main/input/$str.txt").getLines().toList
+}
+
