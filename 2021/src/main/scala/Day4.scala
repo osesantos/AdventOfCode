58a4e35e440bc81bb714c075f9d663b34ffb389d
osesantos <happycod3r@gmail.com>
2021-12-03 08:52:42 +0000

    setup day 4

---
+++ 2021/src/main/scala/Day4.scala
@@ -0,0 +1,15 @@
+def day4(): Unit =
+  val input = "day4".getInput
+  day4_1(input)
+  day4_2(input)
+
+def day4_1(input: List[String]): Unit =
+  println(s"Day 4 - PART 1: ")
+
+def day4_2(input: List[String]): Unit =
+  println(s"Day 4 - PART 2: ")
+
+
+
+
+
