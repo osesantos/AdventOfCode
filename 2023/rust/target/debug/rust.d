166e12afe49516bc933aca2ab5e07b632d99513e
osesantos <jfnfsantos@gmail.com>
2023-12-01 01:05:43 +0000

    Adding 2022 and bootstrap 2023

--- 2023/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/osesantos/git/AdventOfCode/2023/rust/target/debug/rust: /home/osesantos/git/AdventOfCode/2023/rust/src/day1/mod.rs /home/osesantos/git/AdventOfCode/2023/rust/src/main.rs /home/osesantos/git/AdventOfCode/2023/rust/src/utils/mod.rs
