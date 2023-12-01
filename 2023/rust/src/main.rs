9e4b1edf075ff20a9b94ff56c8c36a18d2eca2cb
osesantos <jfnfsantos@gmail.com>
2023-12-01 22:43:25 +0000

    bootstraping day 2

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -2,6 +2,7 @@ use crate::{day1::{day1_1, day1_2}, utils::{get_input_sample, get_input}};
 
 mod utils;
 mod day1;
+mod day2;
 
 fn main() {
     println!("--------------------------");
@@ -11,5 +12,10 @@ fn main() {
     println!("day1.1        - {0}", day1_1(&get_input("day1")));
     println!("day1.2 sample - {0}", day1_2(&get_input_sample("day1_2")));
     println!("day1.2        - {0}", day1_2(&get_input("day1")));
+    println!("");
+    println!("day2.1 sample - {0}", day1_1(&get_input_sample("day2")));
+    println!("day2.1        - {0}", day1_1(&get_input("day2")));
+    println!("day2.2 sample - {0}", day1_2(&get_input_sample("day2")));
+    println!("day2.2        - {0}", day1_2(&get_input("day2")));
     println!("");
 }
