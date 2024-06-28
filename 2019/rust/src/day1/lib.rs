63a9b35815d873ca3b2d2681badf0efab7687596
José Santos <jose.dos.santos@outsystems.com>
2024-06-28 18:16:05 +0100

    2019 day 1 - rust

---
+++ 2019/rust/src/day1/lib.rs
@@ -0,0 +1,69 @@
+pub fn day1() {
+    let input = include_str!("input.txt");
+    println!("day1.1 - {}", execute_part_1(input));
+    println!("day1.2 - {}", execute_part_2(input));
+}
+
+fn execute_part_1(input: &str) -> i32 {
+    let input = input
+        .lines()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut sum = 0;
+
+    for i in input.iter() {
+        sum += formula(i);
+    }
+
+    sum
+}
+
+fn execute_part_2(input: &str) -> i32 {
+    let input = input
+        .lines()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut sum = 0;
+
+    for i in input.iter() {
+        sum += recursive_formula(i);
+    }
+
+    sum
+}
+
+fn formula(mass: &i32) -> i32 {
+    mass / 3 - 2
+}
+
+fn recursive_formula(mass: &i32) -> i32 {
+    let fuel = formula(mass);
+
+    if fuel <= 0 {
+        return 0;
+    }
+
+    fuel + recursive_formula(&fuel)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn assert_part_1() {
+        assert_eq!(execute_part_1("12"), 2);
+        assert_eq!(execute_part_1("14"), 2);
+        assert_eq!(execute_part_1("1969"), 654);
+        assert_eq!(execute_part_1("100756"), 33583);
+    }
+
+    #[test]
+    fn assert_part_2() {
+        assert_eq!(execute_part_2("12"), 2);
+        assert_eq!(execute_part_2("1969"), 966);
+        assert_eq!(execute_part_2("100756"), 50346);
+    }
+}
---
+++ 2019/rust/src/main.rs
@@ -0,0 +1,5 @@
+include!("../src/day1/lib.rs");
+
+fn main() {
+    day1();
+}
