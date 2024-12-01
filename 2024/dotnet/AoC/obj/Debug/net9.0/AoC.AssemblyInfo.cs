d470ffc93ee09a46b67e80116b88c86e65883263
Jose Santos <jfnfsantos@gmail.com>
2024-12-01 06:30:12 +0000

    create generator

--- 2024/dotnet/AoC/obj/Debug/net9.0/AoC.AssemblyInfo.cs
+++ 2024/dotnet/AoC/obj/Debug/net9.0/AoC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AoC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c81a760111f445d94214a7f1a27e146f9b9e8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530c8cbd3ffebd9d80162f5f3f70b941bbd0eda9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AoC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AoC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
