bf06412580dadcc24624c85d437cf0ce1167cb08
Jose Santos <jfnfsantos@gmail.com>
2024-12-03 23:58:02 +0000

    bootstrap day 4

---
+++ 2024/dotnet/AoC/Days/Day4/Day4.cs
@@ -0,0 +1,26 @@
+using AoC.Utils;
+
+namespace AoC.Days.Day4;
+
+/// <summary>
+/// Day4 used for Day class generation
+/// When using replace Day4 with the day
+/// </summary>
+public static class Day4 {
+
+    private static string[] Lines => "Day4".GetInputLines();
+
+    public static void Run() {
+        Console.WriteLine("Day4");
+        Console.WriteLine($"Part1 - {Part1(Lines)}");
+        Console.WriteLine($"Part2 - {Part2(Lines)}");
+    }
+
+    public static int Part1(string[] input) {
+        return 0;
+    }
+
+    public static int Part2(string[] input) {
+        return 0;
+    }
+}
