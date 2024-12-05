ca5ecfb878a30ce7663787063e26310c09eb3682
José Santos <jose.dos.santos@outsystems.com>
2024-12-05 06:44:12 +0000

    day 4 part 2

--- 2024/dotnet/AoC.UnitTests/Day4Tests/Day4.Tests.cs
+++ 2024/dotnet/AoC.UnitTests/Day4Tests/Day4.Tests.cs
@@ -25,6 +25,19 @@ public static class Day4Tests {
 
     [Test]
     public static void TestPart2() {
-        Assert.That(Day4.Part2([]), Is.EqualTo(0));
+        var input = $"""
+                     MMMSXXMASM
+                     MSAMXMSMSA
+                     AMXSXMAAMM
+                     MSAMASMSMX
+                     XMASAMXAMM
+                     XXAMMXXAMA
+                     SMSMSASXSS
+                     SAXAMASAAA
+                     MAMMMXMMMM
+                     MXMXAXMASX
+                     """.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        Assert.That(Day4.Part2(input), Is.EqualTo(9));
     }
 }
