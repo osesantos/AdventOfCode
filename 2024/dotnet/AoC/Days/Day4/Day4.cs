b2e91eed769335fd3106d751fc5f36e3aac651b8
José Santos <jose.dos.santos@outsystems.com>
2024-12-05 06:43:37 +0000

    day 4 part 2

--- 2024/dotnet/AoC/Days/Day4/Day4.cs
+++ 2024/dotnet/AoC/Days/Day4/Day4.cs
@@ -20,122 +20,76 @@ public static class Day4 {
     }
 
     public static int Part1(string[] input) {
+        var rows = input.Length;
+        var cols = input[0].Length;
+
+        const string word = "XMAS";
+        var wordLength = word.Length;
+
         var count = 0;
 
-        // Get horizontal XMAS count
-        count += input.Sum(x => x.CountMatches());
-
-        // Get horizontal Reverse XMAS count
-        count += input.Sum(x => x.CountMatchesReverse());
-
-        // Get vertical XMAS count
-        count += input.ConvertToMatrix().RotateClockWise().ConvertToLines().Sum(x => x.CountMatches());
-
-        // Get vertical Reverse XMAS count
-        count += input.ConvertToMatrix().RotateClockWise().ConvertToLines().Sum(x => x.CountMatchesReverse());
-
-        // Get diagonal XMAS count
-        count += input.ConvertToMatrix().RotateClockwise45().ConvertToLines().Sum(x => x.CountMatches());
-
-        // Get diagonal Reverse XMAS count
-        count += input.ConvertToMatrix().RotateClockwise45().ConvertToLines().Sum(x => x.CountMatchesReverse());
-
-        // Get diagonal XMAS count
-        count += input.ConvertToMatrix().RotateClockwise45(3).ConvertToLines().Sum(x => x.CountMatches());
-
-        // Get diagonal Reverse XMAS count
-        count += input.ConvertToMatrix().RotateClockwise45(3).ConvertToLines().Sum(x => x.CountMatchesReverse());
-
+        // Check all possible starting positions
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                // Check all directions: horizontal, vertical, diagonal
+                for (var dx = -1; dx <= 1; dx++) {
+                    for (var dy = -1; dy <= 1; dy++) {
+                        if (dx == 0 && dy == 0) continue; // Skip the current cell
+                        int x = i, y = j;
+                        int k;
+                        for (k = 0; k < wordLength; k++) {
+                            if (x < 0 || x >= rows || y < 0 || y >= cols || input[x][y] != word[k])
+                            {
+                                break;
+                            }
+                            x += dx;
+                            y += dy;
+                        }
+                        if (k == wordLength) { // Found the word
+                            Console.WriteLine($"Found {word} starting at position ({i}, {j}) in direction ({dx}, {dy})");
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
         return count;
     }
 
     public static int Part2(string[] input) {
-        return 0;
-    }
+        var rows = input.Length;
+        var cols = input[0].Length;
 
-    private static int CountMatchesReverse(this string line) {
-        return Regex.Matches(line, @"SAMX").Count;
-    }
+        var count = 0;
 
-    private static int CountMatches(this string line) {
-        return Regex.Matches(line, @"XMAS").Count;
-    }
-
-    private static char[,] RotateClockwise45(this char[,] matrix, int times = 1) {
-        var newMatrix = matrix;
-        
-        for (var i = 0; i < times; i++) {
-            newMatrix = Rotate();
-        }
-
-        return newMatrix;
-        
-        char[,] Rotate() {
-            var n = matrix.GetLength(0); // Assuming a square matrix
-            var newLength = 2 * n - 1;
-            var rotated = new string[newLength];
-
-            // Initialize the rotated array
-            for (var i = 0; i < newLength; i++) {
-                rotated[i] = new string(' ', newLength);
-            }
-
-            for (var i = 0; i < n; i++) {
-                for (var j = 0; j < n; j++) {
-                    var newRow = i + j;
-                    var newCol = n - 1 + (j - i);
-
-                    var rowChars = rotated[newRow].ToCharArray();
-                    rowChars[newCol] = matrix[i, j];
-                    rotated[newRow] = new string(rowChars);
+        // Check all possible starting positions
+        for (var i = 1; i < rows - 1; i++) {
+            for (var j = 1; j < cols - 1; j++) {
+                if (input[i][j] == 'A') {
+                    // Check for the X-MAS pattern
+                    if (input[i - 1][j - 1] == 'M' && input[i + 1][j + 1] == 'S' &&
+                        input[i + 1][j - 1] == 'M' && input[i - 1][j + 1] == 'S') {
+                        count++;
+                        Console.WriteLine($"Found X-MAS pattern at ({i},{j})");
+                    }
+                    if (input[i - 1][j - 1] == 'M' && input[i + 1][j + 1] == 'S' &&
+                        input[i + 1][j - 1] == 'S' && input[i - 1][j + 1] == 'M') {
+                        count++;
+                        Console.WriteLine($"Found X-MAS pattern at ({i},{j})");
+                    }
+                    if (input[i - 1][j - 1] == 'S' && input[i + 1][j + 1] == 'M' &&
+                        input[i + 1][j - 1] == 'S' && input[i - 1][j + 1] == 'M') {
+                        count++;
+                        Console.WriteLine($"Found X-MAS pattern at ({i},{j})");
+                    }
+                    if (input[i - 1][j - 1] == 'S' && input[i + 1][j + 1] == 'M' &&
+                        input[i + 1][j - 1] == 'M' && input[i - 1][j + 1] == 'S') {
+                        count++;
+                        Console.WriteLine($"Found X-MAS pattern at ({i},{j})");
+                    }
                 }
             }
-
-            return rotated.ConvertToMatrix();
         }
-    }
-
-    private static char[,] ConvertToMatrix(this string[] lines) {
-        var matrix = new char[lines.Length, lines.Length];
-        for (var i = 0; i < lines.Length; i++) {
-            for (var j = 0; j < lines.Length; j++) {
-                matrix[i, j] = lines[i][j];
-            }
-        }
-        return matrix;
-    }
-    
-    private static string[] ConvertToLines(this char[,] matrix) {
-        var lines = new string[matrix.GetLength(0)];
-        for (var i = 0; i < matrix.GetLength(0); i++) {
-            var sb = new StringBuilder();
-            for (var j = 0; j < matrix.GetLength(1); j++) {
-                sb.Append(matrix[i, j]);
-            }
-            lines[i] = sb.ToString();
-        }
-        return lines.Select(line => line.Replace(" ", "")).ToArray();
-    }
-
-    private static char[,] RotateClockWise(this char[,] matrix, int times = 1) {
-        var newMatrix = matrix;
-        
-        for (var i = 0; i < times; i++) {
-            newMatrix = Rotate();
-        }
-
-        return newMatrix;
-
-        char[,] Rotate() {
-            var n = matrix.GetLength(0);
-            var rotated = new char[n, n];
-            for (var i = 0; i < n; i++) {
-                for (var j = 0; j < n; j++) {
-                    rotated[i, j] = matrix[n - j - 1, i];
-                }
-            }
-
-            return rotated;
-        }
+        return count;
     }
 }
